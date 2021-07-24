c78c3734cb109567f9c7e1d5419e9c1ebd4b8181
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-07-24 11:47:41 +0300

    finished slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ class Header extends React.Component {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="home">Home</Nav.Link>
                                 <Nav.Link href="#link">Profile</Nav.Link>
                                 <Nav.Link href="#link">Contact Us</Nav.Link>
                                 <Nav.Link href="#link">Login</Nav.Link>
--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react'
-import { Card } from 'react-bootstrap'
+import React, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import '../components(CSS)/HomePage.css';
 
 export class SearchMovie extends Component {
     render() {
         return (
-            <div>
+            <div className='searchDiv'>
                 {this.props.showSearch &&
                     this.props.movieSearchData.map((movie, index) => {
                         return (
-                            <Card key={index} style={{ width: '18rem' }}>
-                                <Card.Img variant="top"
-                                    onClick={() => this.props.getTrailerByMovieId(index)}
+                            <Card key={index} style={{ width: '18rem' }} className='slider-card'>
+                                <Card.Img variant="top" className='card-image'
+                                    onClick={() => this.props.getTrailerForSearch(index)}
                                     src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                                <div className='details'>
+                                    <h2>{movie.title}</h2>
+                                </div>
                             </Card>
                         )
                     })
