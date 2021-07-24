3fdf614d21cd60756d70a23df68ba0ee81ea2814
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-07-24 16:24:10 +0300

    fix route

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,38 @@
-import React, { Component } from "react";
-// import "../components(CSS)/Header";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { withAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import "../components(CSS)/Header.css";
-// import img
 import logoImg from "../img/movie-land-white-text.png";
-import backgroungImg from "../img/poster1.jpg";
-import Login from './Login'
-// //////////////////////////////////////////////
+
+
 class Header extends React.Component {
   render() {
-    const { isAuthenticated } = this.props.auth0;
     return (
       <div className="header">
 
         <Navbar bg="light" expand="lg" className="nav-bar">
           <Container className="Container">
-            <Navbar.Brand href="#home">
+            <Link to="/">
               <div className="logoImg">
                 <img src={logoImg} alt="Logo" style={{ width: "160px" }} />
               </div>
 
-            </Navbar.Brand>
+            </Link>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Link to="/HomePage">Home</Link>
-                <Link to="/Profile">Profile</Link>
+                <Link to="/">Home</Link>
+               {this.props.auth0.isAuthenticated && <Link to="/Profile">Profile</Link>} 
                 <Link to="/ContactUs">ContactUs</Link>
-                {isAuthenticated === true ? <LogoutButton /> : ""}
-                <div className="logInDiv">
-                  <Login />
-                </div>
-                {/* 
-                {isAuthenticated ? <LoginButton /> : <LogoutButton/> } */}
+               {this.props.auth0.isAuthenticated ? <LogoutButton/> : <LoginButton/>}
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        {/* <div>
-          <img src={backgroungImg} alt="back ground img" />
-        </div> */}
       </div>
     );
   }
